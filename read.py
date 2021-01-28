500277741988b961498d6c6488132f362549a2bc
JY-Yangs <wbhyex@gmail.com>
2021-01-28 16:03:22 +0800

    calc ave length of reviews

--- read.py
+++ read.py
@@ -8,7 +8,9 @@ with open('reviews.txt','r') as f:
 		# 如果"count"跟"1000"的"餘數等於0"的意思
 		# "%"就是求餘數的運算符號, ex: 1003 % 1000 = 3 ; 10 % 6 = 4 
 			print(count) # 這裡印出來可以看出讀的進度到哪了
-print(len(data))
-print(data[0])
-print('--------------------')
-print(data[1])
+print('檔案讀取完了, 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d) # 就是sum_len = sum_len + len(d)
+print('每一筆留言的平均長度是', sum_len/len(data))
